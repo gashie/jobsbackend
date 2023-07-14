38d7edbe80d26c252957ed5a7cb1d3a2b7331d8a
gashie <richardwilliam60@gmail.com>
2023-07-14 14:07:22 +0000

    db port

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  port:process.env.DB_PORT,
 });
 // Exports the pool variable
 module.exports = pool;
