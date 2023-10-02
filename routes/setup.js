65c5dfef2083358eb6655bd7cfe88978acc6f64a
gashie <richardwilliam60@gmail.com>
2023-10-02 14:28:35 +0000

    view job details

--- routes/setup.js
+++ routes/setup.js
@@ -20,7 +20,7 @@ const { CreateQuestionnaire, CreateBulkQuestionnaire, LinkQuestionnaire, DeleteL
 
 const { CreateJobStatus, UpdateJobStatus, ViewJobStatus } = require("../controllers/jobs/jobstatus");
 const { CreateIndustry, ViewIndustry, UpdateIndustry, ViewCompany } = require("../controllers/company/industry");
-const { CreateJobInfo, UpdateJobInfo, AdminApproveJobInfo, ViewMyJobs,ViewJobDetails, ViewJobsData, ApplyJob, ApproveJobApplication, ViewJobApplications, ViewMyJobApplications, ViewMyShortlistedJobApplicants, AdminListJobs } = require("../controllers/jobs/jobinfo");
+const { CreateJobInfo, UpdateJobInfo, AdminApproveJobInfo, ViewMyJobs,ViewJobDetails, ViewJobsData, ApplyJob, ApproveJobApplication, ViewJobApplications, ViewMyJobApplications, ViewMyShortlistedJobApplicants, AdminListJobs, AdminViewJobDetails ,EmoloyerViewJobDetails} = require("../controllers/jobs/jobinfo");
 
 const { CreateBanner, ViewBanners, UpdateBanner } = require("../controllers/admin/banner");
 const { CreateFeed,UpdateFeed,ViewFeeds } = require("../controllers/admin/feed");
@@ -147,6 +147,9 @@ router.route("/createresume").post(protect, CreateResume);
 router.route("/updateresume").post(protect, findResume, UpdateCv);
 router.route("/myresume").post(protect, ViewMyCv);
 router.route("/viewprofile").post(protect, ViewMyProfile);
+
+
+
 //manage coverletter
 router.route("/createcv").post(protect, CreateCoverLetter);
 router.route("/updatecv").post(protect, UpdateCoverLetter);
@@ -168,7 +171,8 @@ router.route("/public/jobdata").post(ViewJobsData);
 router.route("/apply").post(protect,findJobBeforeApply,ApplyJob);
 router.route("/approveapplication").post(protect,findApplicationBeforeApprove,ApproveJobApplication);
 router.route("/admin/viewapplications").post(protect,ViewJobApplications);
-router.route("/employer/viewmyapplications").post(protect,ViewMyJobApplications);
+router.route("/admin/viewjob").post(protect,AdminViewJobDetails);
+router.route("/employer/viewjob").post(protect,EmoloyerViewJobDetails);
 router.route("/employer/viewmyshortlisted").post(protect,findJob,ViewMyShortlistedJobApplicants);
 router.route("/jobseeker/viewmyappliedjobs").post(protect,ViewMyAppliedJobs);
 
